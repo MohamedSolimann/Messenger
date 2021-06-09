7d9526a1542933e18c85b7a23d071077ff153422
MohamedSolimann <mohamadico@gmail.com>
2021-06-09 15:55:13 +0200

    created user model

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const userRoutes = require("./Routes/userRouter");
 
 app.get("/", (req, res) => {
   res.send("hi");
 });
+
+app.use("/user", userRoutes);
 
 app.listen(8000, () => {
   console.log("up and running ...");
